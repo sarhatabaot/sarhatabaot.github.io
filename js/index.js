3676616a4ba8664eca5812cc37a75d5c1f3a5190
Omer Oreg <omeroreg@gmail.com>
2024-04-05 09:09:21 +0300

    Added modrinth links & updates copyright

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ class Resource {
 const endpoint = "https://api.spiget.org/v2/authors/43276/resources";
 // const test_response = './test-response.json';
 
-window.onload = fetchJson(endpoint);
+window.onload = fetchJsonAndApplyToDom(endpoint);
 
 function applyDownloadsToDom(downloads) {
     document.getElementById('downloads').innerHTML = new Intl.NumberFormat().format(downloads);
 }
 
-function fetchJson(path) {
+function fetchJsonAndApplyToDom(path) {
     fetch(path, {
             headers: {
                 'Content-Type': 'application/json',
@@ -38,7 +38,7 @@ function fetchJson(path) {
             //     resources.push(new Resource(data[i].id, data[i].downloads))
             // }
 
-            var totalDownloads = getTotalDownloads(resources);
+            var totalDownloads = getTotalDownloadsFromSpigot(resources);
             applyDownloadsToDom(totalDownloads);
         })
         .catch((err) => console.error(err));
@@ -58,7 +58,7 @@ async function getResourcesFromJson(promiseArray) {
     return resources;
 }
 
-function getTotalDownloads(resources) {
+function getTotalDownloadsFromSpigot(resources) {
     var total = 0;
     resources.forEach(resource => total += resource.downloads);
     return total;
